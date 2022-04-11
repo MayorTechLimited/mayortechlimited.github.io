aa285cb66c465bd614893b71c306d2166fb6b083
William Mayor <mail@mayortech.co.uk>
2022-04-11 23:26:55 +0100

    Don't remove the CNAME file on deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,12 +2,13 @@
 
 . venv/bin/activate
 
+rm -rf output
 pelican content -o output -s publishconf.py
 
 cd output
 rm -rf author blog/ category/ feeds/ theme/ archives.html authors.html  categories.html  tags.html
 cd -
 
-ghp-import output -b main
+ghp-import output --branch=main --cname=www.mayortech.co.uk
 
 git push origin main:main
