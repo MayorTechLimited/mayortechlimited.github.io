2ef05c37bacb59e80560bcdc4ed15de5940ab768
William Mayor <mail@mayortech.co.uk>
2025-03-03 23:36:06 +0000

    Custom deploy script??

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 # ---
 # help-text: Deploy the site
+# environent:
+#   - HOME
+#   - USER
 # ---
 set -eu
 
-. venv/bin/activate
-
 rm -rf dist
-python build.py
-tailwindcss -i ./styles.css -o ./dist/styles.css --minify
+vg compose run --remove-orphans css /tailwindcss -i ./styles.css -o ./dist/styles.css --minify
+vg compose run --remove-orphans static /venv/bin/python watch_static.py --build
+vg compose run --remove-orphans templates /venv/bin/python watch_templates.py --build
 
-ghp-import dist --branch=main --cname=www.mayortech.co.uk
-
+TMP=$(mktemp --directory)
+cp -r dist/* "$TMP"/
+git stash push --all
 git checkout main
+
+function cleanup() {
+    rm -rf "$TMP"
+    git checkout develop
+    git stash pop
+}
+trap cleanup EXIT
+
+rm -rf ./*
+cp -r "$TMP"/* ./
+echo 'www.mayortech.co.uk' > CNAME
+
+D=$(date +"%Y-%m-%dT%H:%M:%S")
+git commit -am "Deploy on $D"
 git push origin main
-git checkout develop
